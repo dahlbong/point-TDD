f95c3d032248dfac54368655ebc610e5f2732048
dahyun <halover111@gmail.com>
2025-03-26 02:34:30 +0900

    [refactor] OOP 준수하여 UserPoint에 잔액 계산 로직 이관

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -28,34 +28,38 @@ public class PointService {
     public UserPoint chargePointOf(long userId, long amount) {
         // 충전 금액 검증
         PointValidator.validateChargeAmount(amount);
-        long currentPoint = currentPointOf(userId);
-        // 잔액 검증
-        PointValidator.validateChargeBalance(currentPoint, amount);
-        long balance = currentPoint + amount;
 
-        return updatePointBalance(userId, balance, TransactionType.CHARGE, amount);
+        // 잔액 검증
+        UserPoint current = getUserOf(userId);
+        PointValidator.validateChargeBalance(current.point(), amount);
+        UserPoint charged = current.charge(amount);
+
+        return updatePointBalance(charged, TransactionType.CHARGE, amount);
     }
 
     // 포인트 사용
     public UserPoint usePointOf(long userId, long amount) {
         // 사용 금액 검증
         PointValidator.validateUseAmount(amount);
-        long currentPoint = currentPointOf(userId);
+
         // 잔액 검증
-        PointValidator.validateSufficientBalance(currentPoint, amount);
-        long balance = currentPoint - amount;
-        return updatePointBalance(userId, balance, TransactionType.USE, amount);
+        UserPoint current = getUserOf(userId);
+        PointValidator.validateSufficientBalance(current.point(), amount);
+        UserPoint used = current.use(amount);
+
+        return updatePointBalance(used, TransactionType.USE, amount);
+    }
+
+    public UserPoint getUserOf(long userId) {
+        UserPoint current = userPointTable.selectById(userId);
+        return current;
     }
 
     // 포인트 사용 or 충전 시 잔액 갱신
-    public UserPoint updatePointBalance(long userId, long newBalance, TransactionType type, long transactionAmount) {
-        UserPoint updatedPoint = userPointTable.insertOrUpdate(userId, newBalance);
-        pointHistoryTable.insert(userId, transactionAmount, type, updatedPoint.updateMillis());
+    public UserPoint updatePointBalance(UserPoint userPoint, TransactionType type, long transactionAmount) {
+        UserPoint updatedPoint = userPointTable.insertOrUpdate(userPoint.id(), userPoint.point());
+        pointHistoryTable.insert(userPoint.id(), transactionAmount, type, updatedPoint.updateMillis());
         return updatedPoint;
-    }
-
-    public long currentPointOf(long userId) {
-        return getPointOf(userId).point();
     }
 
 }
